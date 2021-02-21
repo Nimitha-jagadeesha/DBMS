da9b81da57bb3a8a232fd50c262f075650f1fdc9
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-02-21 21:46:27 +0530

    resize

--- project/settings.py
+++ project/settings.py
@@ -124,9 +124,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 CRONJOBS = [
     ('*/1 * * * *', 'ForestManagement.cron.mycronjob')
 ]
