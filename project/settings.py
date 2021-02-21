8e26b6df7a2266ed651c938b5d4b50126a294bd5
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-02-21 22:10:45 +0530

    Commit

--- project/settings.py
+++ project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
+STATIC_URL = '../ForestManagement/static'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_ROOT =  os.path.join(BASE_DIR, '../ForestManagement/static')
 STATICFILES_DIRS = (
