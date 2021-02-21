a41b96d9295f0efc49d3c1e70a97d8072f8a97b3
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-02-21 21:39:20 +0530

    Commit

--- project/settings.py
+++ project/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL =  os.path.join(BASE_DIR, '/static')
+STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'ForestManagement/static')
 
 CRONJOBS = [
     ('*/1 * * * *', 'ForestManagement.cron.mycronjob')
