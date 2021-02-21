dd557f47f006cc10cceeeb64c8489ca21bf6e042
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-02-21 21:59:19 +0530

    Commit

--- project/settings.py
+++ project/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 CRONJOBS = [
     ('*/1 * * * *', 'ForestManagement.cron.mycronjob')
 ]
